6998984c535f5b301392bec12aca733dc8412f32
ZScomnet <dark6ro@naver.com>
2021-01-19 16:50:34 +0900

    Last Update 21.01.19

--- creator/creator/urls.py
+++ creator/creator/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
-from member.views import kakao_login,kakao_callback
+from member.views import kakao_login,kakao_callback,kakao_join
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('account/',include('allauth.urls')),
     path('account/login/kakao/',kakao_login,name='kakao_login'),
-    path('account/login/kakao/callback',kakao_callback,name='kakao_callback'),
+    path('account/login/kakao/callback/',kakao_callback,name='kakao_callback'),
+  	path('account/join/kakao/',kakao_join,name='kakao_join'),
     # path('account/registration/',include('rest_auth.registration.urls')),
     # path('account/',include('rest_auth_urls')),
     # url(r'account/registration/confirm-email/(?P<key>.+)/$',confirm_email,name="confirm_email"),
